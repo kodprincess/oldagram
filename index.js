085659f12583b374416125957281cd181470b93e
kodprincess <klara.trojmiasto@gmail.com>
2024-02-19 12:17:48 +0100

    Increase the likes number when clicking on an image or a heart icon.

--- index.js
+++ index.js
@@ -38,15 +38,48 @@ function setValueTo(node, className, value) {
     }
 }
 
+function isLiked(storageId) {
+    return localStorage.getItem(storageId)
+}
+
+function renderLikeIcon(storageId, postEl) {
+    if(isLiked(storageId)) {
+        postEl.getElementsByClassName("like-action")[0].classList.add("like-action-selected")
+    }
+}
+
 function renderPosts() {
     for(let i=0; i < posts.length; i++) {
         let postEl = postTemplate.cloneNode(true)
 
+        // fill post with images and data
         keys.forEach(function(key) {
             setValueTo(postEl, `post-${key}`, posts[i][key])
         })
         
         mainEl.appendChild(postEl)
+        
+        // check if the post is liked
+        let storageId = `likePost${i}`
+        renderLikeIcon(storageId, postEl)
+
+        // increase likes on dbl click
+        let image = postEl.getElementsByClassName("post-image-container")[0]        
+        let likeBtn = postEl.getElementsByClassName("like-action")[0]
+        let likableElements = [image, likeBtn] 
+
+        likableElements.forEach(function(element) {
+            element.addEventListener("dblclick", function() {
+                if(!isLiked(storageId)) {
+                    let likesEl = postEl.getElementsByClassName("post-likes")[0]
+                    likesEl.textContent = Number(likesEl.textContent) + 1
+                    
+                    localStorage.setItem(storageId, "true")
+
+                    renderLikeIcon(storageId, postEl)
+                }
+            })
+        })
     }
 }
 
@@ -55,5 +88,7 @@ const postTemplate = document.getElementsByClassName("post")[0]
 mainEl.innerHTML = ""
 
 let keys = Object.keys(posts[0])
+
+localStorage.clear()
 
 renderPosts()
